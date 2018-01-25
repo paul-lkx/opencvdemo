a0df4545680127980f0ad991ff0f1957f0d4815a
shye <464716642@qq.com>
2018-01-25 16:05:04 +0800

    20180125

---
+++ canny_edge_detector.py
@@ -0,0 +1,14 @@
+#!/bin/lib/python
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('kobe.jpg',0)
+edges = cv2.Canny(img,100,250)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.show()
